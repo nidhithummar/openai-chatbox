af94637620e00cf187a81dfc2da2752b99adf707
Nidhi <nidhithummar1010@gmail.com>
2024-07-27 22:32:36 -0700

    first commit

---
+++ backend/dist/config/openai-config.js
@@ -0,0 +1,9 @@
+import { Configuration } from "openai";
+export const configureOpenAI = () => {
+    const config = new Configuration({
+        apiKey: process.env.AI_SECRET,
+        organization: process.env.AI_ORGANIZATION,
+    });
+    return config;
+};
+//# sourceMappingURL=openai-config.js.map
---
+++ backend/src/utils/openai-config.ts
@@ -0,0 +1,11 @@
+import {Configuration} from 'openai';
+
+
+ export const configureOpenAi=()=>{
+    const conifg = new Configuration({
+        apiKey:process.env.OPEN_AI_SECRET,
+        organization:process.env.OPENAI_ORAGANIZATION,
+
+    });
+    return conifg;
+}
